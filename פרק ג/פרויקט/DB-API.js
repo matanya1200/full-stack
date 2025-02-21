63a56cf01c874e10f71ae818f4dcd4f6cef2cd70
Shmuel Amir <shmulik5777@gmail.com>
2025-02-21 11:43:37 +0200

    remove static methods. add searchBy method.

---
+++ פרק ג/פרויקט/DB-API.js
@@ -0,0 +1,67 @@
+class Database {
+  constructor(dbName) {
+    this.dbName = dbName;
+  }
+
+  // ----- private methods -----
+
+  #getDB() {
+    const data = JSON.parse(localStorage.getItem(this.dbName));
+    return Array.isArray(data) ? data : [];
+  }
+
+  #saveDB(data) {
+    localStorage.setItem(this.dbName, JSON.stringify(data));
+  }
+
+  // ----- public methods -----
+
+  searchBy(key, value) {
+    const db = this.#getDB();
+    return db.filter((record) => record[key].includes(value));
+  }
+
+  getAll() {
+    return this.#getDB();
+  }
+
+  getById(id) {
+    const db = this.#getDB();
+
+    const item = db.find((record) => record.id === id);
+    if (!item) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    return item;
+  }
+
+  create(record) {
+    const db = this.#getDB();
+    db.push(record);
+    this.#saveDB(db);
+  }
+
+  update(id, updatedRecord) {
+    const db = this.#getDB();
+
+    const index = db.findIndex((record) => record.id === id);
+    if (index === -1) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    db[index] = { ...db[index], ...updatedRecord };
+    this.#saveDB(db);
+  }
+
+  delete(id) {
+    let db = this.#getDB();
+
+    const filteredDB = db.filter((record) => record.id != id);
+    if (filteredDB.length === db.length) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    this.#saveDB(filteredDB);
+  }
+}
