fe82d661b895f68b6a2f82dcc14cd72831326443
matanya.nabet <mnabet@g.jct.ac.il>
2025-02-24 11:24:11 +0200

    עדכון

--- פרק ג/פרויקט/user-server.js
+++ פרק ג/פרויקט/user-server.js
@@ -1,14 +1,14 @@
 class UserServer {
 
+  static db = new Database("userDB");
+
   //קבלת כל המשתמשים
   static getUsers(){
-    this.db = new Database("userDB");
     return { status: 200, data: this.db.getAll() }
   }
 
   //הוספת משתמש
   static addUser(user) {
-    this.db = new Database("userDB");
     const users = this.db.getAll();
     if (users.find(u => u.id === user.id || u.email === user.email)) {
         return { status: 400, error: "User already exists" };
@@ -19,7 +19,6 @@ class UserServer {
 
   //מחיקת משתמש
   static deleteUser(id){
-    this.db = new Database("userDB");
     this.db.delete(id);
     return { status: 200, message: "User deleted successfully" };
   }
