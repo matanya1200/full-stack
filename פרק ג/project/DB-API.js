36b249f886a8b310fe6f8c399a3f28656bf6bd1a
matanya.nabet <mnabet@g.jct.ac.il>
2025-02-26 12:39:57 +0200

    התקייה project מכילה את הפרויקט הגמור עם כל השינוים שהצעתי אם אתה עדין רוצה לנסות בעצמך אז בתקייה פרויקט יש את הפרויקט לפני השינויים

---
+++ פרק ג/project/DB-API.js
@@ -0,0 +1,92 @@
+class Database {
+  constructor(dbName) {
+    this.dbName = dbName;
+  }
+
+  // ----- private methods -----
+
+  //קבלת בסיס הנתונים
+  #getDB() {
+    const data = JSON.parse(localStorage.getItem(this.dbName));
+    return Array.isArray(data) ? data : [];
+  }
+
+  //שמירת בסיס הנתונים
+  #saveDB(data) {
+    localStorage.setItem(this.dbName, JSON.stringify(data));
+  }
+
+  // ----- public methods -----
+
+  //קבלת כל המשימות
+  getAll() {
+    return this.#getDB();
+  }
+
+  //קבלת משימה
+  getById(id) {
+    const db = this.#getDB();
+
+    const item = db.find((record) => record.id === id);
+    if (!item) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+    
+    return item;
+  }
+
+  //יצירה
+  create(record) {
+    const db = this.#getDB();
+    db.push(record);
+    this.#saveDB(db);
+  }
+
+  //עדכון
+  updateTask(id, updatedRecord) {
+    const db = this.#getDB();
+
+    const index = db.findIndex((record) => record.id === id);
+    if (index === -1) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    db[index] = { ...db[index], ...updatedRecord };
+    this.#saveDB(db);
+  }
+
+  updateUser(id, updatedRecord) {
+    const db = this.#getDB();
+
+    const index = db.findIndex((record) => record.user === id);
+    if (index === -1) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    db[index] = { ...db[index], ...updatedRecord };
+    this.#saveDB(db);
+  }
+
+  //מחיקה 
+  deleteTask(id) {
+    let db = this.#getDB();
+
+    const filteredDB = db.filter((record) => record.id != id);
+    if (filteredDB.length === db.length) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    this.#saveDB(filteredDB);
+  }
+
+  deleteUser(id) {
+    let db = this.#getDB();
+
+    const filteredDB = db.filter((record) => record.user != id);
+    if (filteredDB.length === db.length) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    this.#saveDB(filteredDB);
+  }
+}
