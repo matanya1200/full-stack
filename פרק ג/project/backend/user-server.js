ea5019f33ef2e5cfc1ed415e3d7a46bea3956046
matanya.nabet <mnabet@g.jct.ac.il>
2025-03-06 12:04:03 +0200

    הפכתי את סוגי השגיאות לקבועים ושניתי את הקוד בהתאם

--- פרק ג/project/backend/user-server.js
+++ פרק ג/project/backend/user-server.js
@@ -4,28 +4,28 @@ class UserServer {
 
   //קבלת כל המשתמשים
   static getUsers(){
-    return { status: 200, data: this.db.getAll() }
+    return { status: HTTP_STATUS_CODES.OK, data: this.db.getAll() }
   }
 
   //הוספת משתמש
   static addUser(user) {
     const users = this.db.getAll();
     if (users.find(u => u.user === user.user || u.email === user.email)) {
-        return { status: 400, error: "User already exists" };
+        return { status: HTTP_STATUS_CODES.BAD_REQUEST, error: "User already exists" };
     }
     this.db.create(user)
-    return { status: 201, message: "User registered successfully" };
+    return { status: HTTP_STATUS_CODES.CREATED, message: "User registered successfully" };
   }
 
   //מחיקת משתמש
   static deleteUser(id){
     this.db.deleteUser(id);
-    return { status: 200, message: "User deleted successfully" };
+    return { status: HTTP_STATUS_CODES.OK, message: "User deleted successfully" };
   }
 
   //עדכון משתמש
   static updateUser(id, newId){
     this.db.updateUser(id, newId);
-    return { status: 200, message: "User deleted successfully" };
+    return { status: HTTP_STATUS_CODES.OK, message: "User deleted successfully" };
   }
 }
