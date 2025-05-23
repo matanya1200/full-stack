552596df2139868644b7900cccbfa5b214cf6d69
matanya.nabet <mnabet@g.jct.ac.il>
2025-05-23 13:49:41 +0300

    fix login

--- פרק ה/project-5/src/App.jsx
+++ פרק ה/project-5/src/App.jsx
@@ -2,17 +2,24 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  const user = JSON.parse(localStorage.getItem("user"));
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/home/*" element={user ? <Home /> : <Navigate to="/login" />} />
+        
+        <Route
+          path="/home/*"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
